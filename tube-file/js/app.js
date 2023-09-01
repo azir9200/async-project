28fb14a711c5e4e48c1677b3aeefada3375f3f9f
Azir@9200 <aziruddin83@hotmail.com>
2023-09-01 20:29:00 +0100

    added

--- tube-file/js/app.js
+++ tube-file/js/app.js
@@ -17,16 +17,31 @@ const loadInfo = async () => {
 };
 
 const handleDataLoad = async (categoryid) => {
-  const response = await fetch('https://openapi.programming-hero.com/api/videos/category/${id}');
+  const response = await fetch('https://openapi.programming-hero.com/api/videos/category/${category_id}');
   const data = await response.json();
-  const cardContainer =
+  const cardContainer = document.getElementById('card-container');
 
-    data.data.forEach((news){
-      console.log(news);
-    })
-  console.log("data");
+  data.data.forEach((news) => {
+    const div = document.createElement('div');
+    d.innerHTML = `
+    <div class="card w-96 bg-base-100 shadow-xl">
+        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">
+            Shoes!
+            <div class="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <div class="badge badge-outline">Fashion</div>
+            <div class="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+
+    `;
+    cardContainer.appendChild(div);
+  });
+  //console.log("data");
 };
-
-
-
 loadInfo();
